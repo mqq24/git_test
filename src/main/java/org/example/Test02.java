81a3bda4a718bb4e51e03805454618f0eb6e5b8d
mqq24 <2836740299@qq.com>
2021-08-17 15:04:39 +0800

    dev分支添加Test02

---
+++ src/main/java/org/example/Test02.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author asus
+ * @create 2021-08-17 15:03
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("Test02.....");
+    }
+}
