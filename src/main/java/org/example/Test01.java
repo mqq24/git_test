a495ed32ef5e17815d26f5f01ce8896898035ee6
mqq24 <2836740299@qq.com>
2021-08-17 14:12:46 +0800

    项目初始化

---
+++ src/main/java/org/example/Test01.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author asus
+ * @create 2021-08-17 14:01
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01.....");
+    }
+}
